cf200362046ba865594532ceabe291de8c9f7e02
Rodrigo Rodrigues <roliveiradev@gmail.com>
2019-08-24 00:25:43 -0300

    Adicionado variável ambiente pra URL da API

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://rroweek-backend.herokuapp.com'
-    //baseURL: 'http://localhost:3333'
+    //baseURL: 'https://rroweek-backend.herokuapp.com'
+    baseURL: process.env.REACT_APP_API_URL
 });
 
 export default api;
